f5da540f495e55282d570f0d09c25693a140e46a
林圳源 <2410267884@qq.com>
2019-11-19 22:28:02 +0800

    Done

---
+++ src/Day20191119_JVM/Test.java
@@ -0,0 +1,18 @@
+package Day20191119_JVM;
+
+/**
+ * 静态成员变量加上了final修饰符，再编译阶段会放入到Test的常量池中，run类不会被加载
+ * 助记符： ldc  bipush sipush iconst_1
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(run.str);
+    }
+}
+class run{
+    public static final float str = 1.f;
+    static {
+        System.out.println("123");
+    }
+
+}
