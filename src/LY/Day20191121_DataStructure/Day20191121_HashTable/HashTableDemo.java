1ef2d20cc5357d72399391fcafb2cbd7d0d9cb43
Cynaith <894324762@qq.com>
2019-11-21 16:33:02 +0800

    链表基本操作

---
+++ src/LY/Day20191121_DataStructure/Day20191121_HashTable/HashTableDemo.java
@@ -0,0 +1,46 @@
+package LY.Day20191121_DataStructure.Day20191121_HashTable;
+
+
+/**
+ * @Auther: liuyang
+ * @Date: 2019/11/21 16:20
+ * @Description: 链表基本操作
+ */
+public class HashTableDemo {
+    public static void main(String[] args) {
+        Node head = new Node();
+        Node index = head;
+        int a[] = {1,2,3,4,5,6};
+        for(int i=0;i<a.length;i++){
+            index.setData(a[i]);
+            index.setNext(new Node());
+            index = index.getNext();
+        }
+        index = head;
+        while(index.getNext()!=null){
+            System.out.println(index.getData());
+            index = index.getNext();
+        }
+    }
+}
+class Node{
+    private Node next = null;
+    private int data;
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+
+    public int getData() {
+        return data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
+}
