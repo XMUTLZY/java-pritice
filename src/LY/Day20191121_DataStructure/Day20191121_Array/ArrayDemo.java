5f15f824db89294212b13c641dee59c6c143f00b
Cynaith <894324762@qq.com>
2019-11-21 16:18:46 +0800

    一维至多维数组的创建以及初始化

---
+++ src/LY/Day20191121_DataStructure/Day20191121_Array/ArrayDemo.java
@@ -0,0 +1,50 @@
+package LY.Day20191121_DataStructure.Day20191121_Array;
+
+/**
+ * @Auther: liuyang
+ * @Date: 2019/11/21 16:10
+ * @Description:数组使用
+ */
+public class ArrayDemo {
+    public static void main(String[] args) {
+
+        /**
+         * 基本数据类型的
+         *             整数类型（byte、short、int、long）默认值是0；
+         *
+         *             基本数据类型的浮点类型（float、double）默认值是0.0；
+         *
+         *             基本数据类型的字符类型（char）默认值是'\u0000'；
+         *
+         *             基本数据类型的布尔类型（boolean）默认值是false；
+         *
+         *             类型的引用类型（类、数组、接口、String）默认值是null.
+         */
+
+        /**
+         * 动态初始化:初始化时由程序员指定数组的长度，由系统初始化每个数组元素的默认值。
+         */
+        int a[] = new int[4];
+
+
+        /**
+         * 静态初始化:初始化时由程序员显式指定每个数组元素的初始值,由系统决定数组的长度；
+         */
+        int b[] = new int[]{1,2,3,4};
+
+
+        /**
+         * 简化版静态初始化
+         */
+        int c[] = {1,2,3,4};
+
+        /**
+         * 二维数组
+         */
+        int d[][] = new int[4][4];
+        int []e[] = new int[2][];
+        int [][]f = new int[2][];
+        int [][]g = {{1,2},{1,2}};
+
+    }
+}
