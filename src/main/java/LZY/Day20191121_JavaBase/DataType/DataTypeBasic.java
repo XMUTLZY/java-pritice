05479290b4196e6c459581b54d8728736cf5cb2d
XMUTLZY <48351347+XMUTLZY@users.noreply.github.com>
2019-11-25 15:36:05 +0800

    To Maven Project

---
+++ src/main/java/LZY/Day20191121_JavaBase/DataType/DataTypeBasic.java
@@ -0,0 +1,17 @@
+package LZY.Day20191121_JavaBase.DataType;
+
+/**
+ * @Title: Java8大基本数据类型
+ */
+public class DataTypeBasic {
+    /**
+     * @byte：8位
+     * @short：16位
+     * @int：32位
+     * @long：64位
+     * @float：32位
+     * @double：64位
+     * @boolean：1位
+     * @char：16位
+     */
+}
